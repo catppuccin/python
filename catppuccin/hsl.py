b66109f97eaa5f6fa2e8e9426e13a101b4b12454
backwardspy <backwardspy@pigeon.life>
2023-10-05 23:58:24 +0100

    fix: correct lint & type errors

--- catppuccin/hsl.py
+++ catppuccin/hsl.py
@@ -1,25 +1,30 @@
+"""
+Utilities for working with HSL colours.
+"""
 from __future__ import annotations
 
 from typing import Tuple
 
 
-def rgb_to_hsl(r: int, g: int, b: int) -> Tuple[int, int, int]:
-    r /= 255
-    g /= 255
-    b /= 255
+# pylint: disable=invalid-name
+def rgb_to_hsl(r: int, g: int, b: int) -> Tuple[int, float, float]:
+    """Convert RGB to HSL."""
+    r1 = r / 255
+    g1 = g / 255
+    b1 = b / 255
 
-    cmax = max(r, g, b)
-    cmin = min(r, g, b)
+    cmax = max(r1, g1, b1)
+    cmin = min(r1, g1, b1)
     delta = cmax - cmin
 
     if delta == 0:
-        h = 0
-    elif cmax == r:
-        h = ((g - b) / delta) % 6
-    elif cmax == g:
-        h = (b - r) / delta + 2
+        h = 0.0
+    elif cmax == r1:
+        h = ((g1 - b1) / delta) % 6
+    elif cmax == g1:
+        h = (b1 - r1) / delta + 2
     else:
-        h = (r - g) / delta + 4
+        h = (r1 - g1) / delta + 4
 
     h = round(h * 60)
     if h < 0:
@@ -28,7 +33,7 @@ def rgb_to_hsl(r: int, g: int, b: int) -> Tuple[int, int, int]:
     l = (cmax + cmin) / 2
 
     if delta == 0:
-        s = 0
+        s = 0.0
     else:
         s = delta / (1 - abs(2 * l - 1))
 
