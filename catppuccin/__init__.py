2e4b82e3b8779c8bc47eab0f34862337b4ea4b23
miloth <milo_knightword@gmail.com>
2024-04-26 15:34:50 +0100

    Port of the current `matplotlib` code

--- catppuccin/__init__.py
+++ catppuccin/__init__.py
@@ -1,4 +1,13 @@
 """🐍 Soothing pastel theme for Python."""
+
+import importlib.util
+
+from catppuccin.extras.matplotlib import _register_colormap_list, _register_styles
 from catppuccin.palette import PALETTE
 
 __all__ = ["PALETTE"]
+
+# Attempt to register styles and colormaps if matplotlib is available
+if importlib.util.find_spec("matplotlib") is not None:
+    _register_styles()
+    _register_colormap_list()
---
+++ catppuccin/extras/matplotlib.py
@@ -0,0 +1,49 @@
+"""Soothing pastel theme for matplotlib"""
+
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import cast
+
+import matplotlib as mpl
+import matplotlib.colors
+import matplotlib.style
+
+
+def _load_palette_json() -> dict[str, dict[str, dict[str, str]]]:
+    # TODO: kill this function and load from actual PALETTE objects.  # noqa: TD002, TD003, FIX002, E501
+    with (CATPPUCCIN_STYLE_DIRECTORY / "palettes.json").open() as f:
+        return cast(dict[str, dict[str, dict[str, str]]], json.load(f))
+
+
+CATPPUCCIN_STYLE_DIRECTORY = Path(__file__).parent / "matplotlib_styles"
+MPL_PALETTES = _load_palette_json()
+
+
+def _register_styles() -> None:
+    """Register the included stylesheets in the mpl style library."""
+    catppuccin_stylesheets = mpl.style.core.read_style_directory(  # type: ignore [attr-defined]
+        CATPPUCCIN_STYLE_DIRECTORY
+    )
+    mpl.style.core.update_nested_dict(mpl.style.library, catppuccin_stylesheets)  # type: ignore [attr-defined]
+
+
+def _register_colormap_list() -> None:
+    """Register the included color maps in the `matplotlib` colormap library."""
+    for palette_name in ["latte", "frappe", "macchiato", "mocha"]:
+        cmap = get_colormap_from_list(palette_name, ["base", "blue"])
+        mpl.colormaps.register(cmap=cmap, name=palette_name)
+
+
+def get_colormap_from_list(
+    palette_name: str, color_names: list[str]
+) -> matplotlib.colors.LinearSegmentedColormap:
+    """# TODO: add proper description."""
+    color_list = [load_color(palette_name, color_name) for color_name in color_names]
+    return matplotlib.colors.LinearSegmentedColormap.from_list(palette_name, color_list)
+
+
+def load_color(palette_name: str, color_name: str, *, color_format: str = "hex") -> str:
+    """# TODO: add proper description."""
+    return MPL_PALETTES[palette_name][color_name][color_format]
