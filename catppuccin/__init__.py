315f7ebd77644e454ab168902725e5556dbee89b
miloth <milo.knightword@gmail.com>
2024-06-17 21:29:36 +0100

    bugfix: conditional `matplotlib` library import

--- catppuccin/__init__.py
+++ catppuccin/__init__.py
@@ -2,12 +2,13 @@
 
 import importlib.util
 
-from catppuccin.extras.matplotlib import _register_colormap_list, _register_styles
 from catppuccin.palette import PALETTE
 
 __all__ = ["PALETTE"]
 
 # Attempt to register styles and colormaps if matplotlib is available
 if importlib.util.find_spec("matplotlib") is not None:
+    from catppuccin.extras.matplotlib import _register_colormap_list, _register_styles
+
     _register_styles()
     _register_colormap_list()
