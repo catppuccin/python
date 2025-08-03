38e7e5fb33bbd358f50db3a721e76c01e7e5353c
backwardspy <backwardspy@pigeon.life>
2025-08-03 21:04:54 +0100

    test: fix pygments tests

--- tests/test_pygments.py
+++ tests/test_pygments.py
@@ -5,11 +5,11 @@ from catppuccin.extras.pygments import LatteStyle, MochaStyle
 
 def test_mocha_style_colors() -> None:
     style = MochaStyle()
-    assert style.background_color == "#1e1e2e"
+    assert style.background_color == "#181825"
     assert style.styles[Text] == "#cdd6f4"
 
 
 def test_latte_style_colors() -> None:
     style = LatteStyle()
-    assert style.background_color == "#eff1f5"
+    assert style.background_color == "#e6e9ef"
     assert style.styles[Text] == "#4c4f69"
